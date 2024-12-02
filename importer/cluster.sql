0ff17716d1b6d38215ba60ccdae39f4338bdd925
Hsiao-Ting Yu <sst.dreams@gmail.com>
2024-12-03 05:57:23 +0800

    feat: refine query

--- importer/cluster.sql
+++ importer/cluster.sql
@@ -10,7 +10,8 @@ CREATE MATERIALIZED VIEW address_clusters AS WITH addrs_with_cid AS (
 )
 SELECT ANY_VALUE(city) AS city,
     concat(ANY_VALUE(street), ANY_VALUE(place)) AS label,
-    ST_Buffer(ST_ConvexHull(ST_Collect(geom)), 3) AS geom
+    ST_ConvexHull(ST_ReducePrecision(ST_Collect(geom), 1)) AS geom,
+    COUNT(geom) AS cnt
 FROM addrs_with_cid
 WHERE cp > ''
     and cid is not null
